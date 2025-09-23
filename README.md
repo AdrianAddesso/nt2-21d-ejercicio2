65e92e49e894d7af79df0c6c5a66df991a35a476
adrian.addesso <adrian.addesso@gmail.com>
2025-09-23 19:09:44 -0300

    CDN a CLI completado

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # NT2 - Trabajo Práctico No 2
 
 Realizar las siguientes tareas:
-* [ ] Hacer un fork del repositorio https://github.com/danielsanchez68/nt2-21d-ejercicio2 para
+* [x] Hacer un fork del repositorio https://github.com/danielsanchez68/nt2-21d-ejercicio2 para
 tener el código en la cuenta de Github de cada uno de ustedes, separándolo del versionado
 del repositorio original.
-* [ ] Clonar este repositorio en la máquina local, luego convertir el proyecto Vue CDN a Vue
+* [x] Clonar este repositorio en la máquina local, luego convertir el proyecto Vue CDN a Vue
 CLI con Vite.
 * [ ] Modificar la funcionalidad original, agregando dos filtros de búsqueda: uno por nombre (o
 nombre + apellido) y otro por dni. Pensar que necesitan agregar en el proyecto Vue CLI
--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="container-fluid mt-3" id="app">
+        <main>
+            <Buscador/>
+            <img style ="margin-top: 200px;" src="https://content.imageresizer.com/images/memes/cheems-meme-3nzkub.png" width="100px"></img>
+        </main>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup>
+    import Buscador from './components/Buscador.vue'
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+html, body, #app  {
+    background-color: rgb(239, 228, 228);
 }
 </style>
